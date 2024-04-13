1f22c19fde715b5d278b95090783938167d27a44
Erik Olson <erikamerico1991@gmail.com>
2024-04-13 00:59:19 -0400

    create a countdown until next foosball tournament

---
+++ src/components/Countdown.jsx
@@ -0,0 +1,53 @@
+import { useState, useEffect } from "react";
+
+const Timer = () => {
+  const calculateTimeLeft = () => {
+    const now = new Date();
+    const targetDate = new Date("April 20, 2024 20:00:00 EST");
+
+    const difference = targetDate - now;
+    let timeLeft = {};
+
+    if (difference > 0) {
+      timeLeft = {
+        days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+        hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+        minutes: Math.floor((difference / 1000 / 60) % 60),
+        seconds: Math.floor((difference / 1000) % 60),
+      };
+    }
+
+    return timeLeft;
+  };
+
+  const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(calculateTimeLeft());
+    }, 1000);
+
+    return () => clearTimeout(timer);
+  });
+
+  const timerComponents = [];
+
+  Object.keys(timeLeft).forEach((interval) => {
+    if (!timeLeft[interval]) {
+      return;
+    }
+
+    timerComponents.push(
+      <span key={interval}>
+        {timeLeft[interval]} {interval}{" "}
+      </span>
+    );
+  });
+  return (
+    <div id="timer">
+      {timerComponents.length ? timerComponents : <span>GAME TIME!!</span>}
+    </div>
+  );
+};
+
+export default Timer;
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,10 +1,12 @@
 import "./homePage.css";
+import Timer from "./Countdown";
 
 const HomePage = () => {
   return (
     <>
       <div>
-        <h1>FOOSBALL</h1>
+        <h1>NEXT TOURNAMENT: 4/20/2024 8 PM</h1>
+        <Timer />
       </div>
     </>
   );
