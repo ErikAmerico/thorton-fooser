4500ffc99cc665ab4509ee85d49fd1149dd96e1b
Erik Olson <erikamerico1991@gmail.com>
2024-04-20 23:46:01 -0400

    update with new tourney date and last champ photo

--- src/components/HomePage/Countdown/Countdown.jsx
+++ src/components/HomePage/Countdown/Countdown.jsx
@@ -5,7 +5,7 @@ import "./countdown.css";
 const Timer = () => {
   const calculateTimeLeft = () => {
     const now = moment();
-    const targetDate = moment.tz("2024-04-20 20:00:00", "America/New_York");
+    const targetDate = moment.tz("2024-05-18 20:00:00", "America/New_York");
 
     const difference = targetDate - now;
     let timeLeft = {};
