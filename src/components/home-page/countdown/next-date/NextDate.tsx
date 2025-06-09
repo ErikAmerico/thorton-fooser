38e290f07850bc334f32e6cb382ed6c3c99bfd33
Erik Olson <erikamerico1991@gmail.com>
2025-06-09 09:50:53 -0400

    create component to display the date of the next tournament.
    It will be used in Countdown.tsx and the values will eventually be tied together

---
+++ src/components/home-page/countdown/next-date/NextDate.tsx
@@ -0,0 +1,11 @@
+import "./nextDate.css";
+
+export default function NextDate() {
+  return (
+    <div className="nt-card">
+      <span className="nt-date">
+        NEXT TOURNAMENT: <br /> --/--/---- @ --:--pm
+      </span>
+    </div>
+  );
+}
