09a899019abd61d28ac82d510a3b4748ea2ab7b0
Erik Olson <erikamerico1991@gmail.com>
2025-06-09 16:47:39 -0400

    add react navigation to Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import type { MenuProps } from "antd";
 import { Menu } from "antd";
+import { useNavigate } from "react-router-dom";
 import "./header.css";
 
-type MenuItem = Required<MenuProps>["items"][number];
-
-const items: MenuItem[] = [
+const items = [
   {
     label: "Home",
     key: "home",
@@ -15,21 +14,36 @@ const items: MenuItem[] = [
     label: "Bracket",
   },
   {
-    key: "powerrankings",
+    key: "rankings",
     label: "Power Rankings",
   },
   {
     key: "doc",
     label: "Documentary",
   },
+  {
+    key: "champs",
+    label: "Champions",
+  },
 ];
 
-const Header: React.FC = () => {
+const Header = () => {
   const [current, setCurrent] = useState("mail");
+  const navigate = useNavigate();
 
   const onClick: MenuProps["onClick"] = (e) => {
-    console.log("click ", e);
     setCurrent(e.key);
+    if (e.key === "home") {
+      navigate("/");
+    } else if (e.key === "bracket") {
+      navigate("/bracket");
+    } else if (e.key === "rankings") {
+      navigate("/rankings");
+    } else if (e.key === "doc") {
+      navigate("/documentary");
+    } else if (e.key === "champs") {
+      navigate("/champs");
+    }
   };
 
   return (
