81cc1b33711b2ad5e089304df14772506977c154
Erik Olson <erikamerico1991@gmail.com>
2025-06-11 00:41:55 -0400

    create mock player data

---
+++ src/data/mockPlayers.ts
@@ -0,0 +1,23 @@
+export interface Player {
+  id: string;
+  name: string;
+}
+
+export const mockPlayers: Player[] = [
+  { id: "1", name: "Erik" },
+  { id: "2", name: "Ofir" },
+  { id: "3", name: "Anna" },
+  { id: "4", name: "Michelle" },
+  { id: "5", name: "Dan" },
+  { id: "6", name: "Rachelle J." },
+  { id: "7", name: "Cooper" },
+  { id: "8", name: "Rachel L." },
+  { id: "9", name: "Tommy" },
+  { id: "10", name: "Monica" },
+  { id: "11", name: "C.J" },
+  { id: "12", name: "Andy P." },
+  { id: "13", name: "Rachel P." },
+  { id: "14", name: "Casey" },
+  { id: "15", name: "Jake" },
+  { id: "16", name: "Andraya" },
+];
