8bb8c3a6f53cba3e20d1fddcd0b036f2d157c2eb
Erik Olson <erikamerico1991@gmail.com>
2025-06-19 10:18:04 -0400

    collect secret code in SubmitResultsModal
    disable the submit button if no secretCode has been entered

--- src/components/bracket/_components/SubmitResultsModal.tsx
+++ src/components/bracket/_components/SubmitResultsModal.tsx
@@ -1,23 +1,54 @@
-import { Modal } from "antd";
+import { Modal, Input, Button } from "antd";
 import { SubmitResultsProps } from "../../../types";
+import { useState } from "react";
 
 export default function SubmitResultsModal({
   open,
   onOk,
   onCancel,
 }: SubmitResultsProps) {
+  const [secretCode, setSecretCode] = useState("");
+  const isDisabled = !secretCode;
+
+  const confirm = () => {
+    onOk(secretCode);
+    setSecretCode("");
+  };
+
+  const cancel = () => {
+    setSecretCode("");
+    onCancel();
+  };
+
   return (
     <Modal
       title="Submit Results"
       open={open}
-      onOk={onOk}
-      onCancel={onCancel}
+      onOk={confirm}
+      okButtonProps={{
+        disabled: isDisabled,
+        style: {
+          backgroundColor: isDisabled ? "gray" : "green",
+          borderColor: isDisabled ? "gray" : "green",
+          color: isDisabled ? "black" : "white",
+        },
+      }}
+      onCancel={cancel}
       closable={false}
       okText="Submit"
       style={{ textAlign: "center" }}
     >
       Submit results and update rankings. Cannot update results after
       submission.
+      <div>
+        <Input
+          placeholder="Enter Secret Access Code"
+          type="password"
+          value={secretCode}
+          onChange={(e) => setSecretCode(e.target.value)}
+          style={{ width: "205px" }}
+        />
+      </div>
     </Modal>
   );
 }
