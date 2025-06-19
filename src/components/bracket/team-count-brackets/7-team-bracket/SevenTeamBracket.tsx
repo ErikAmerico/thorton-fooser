a4c20cdee8348a744d819cebc3b13be434b773dd
Erik Olson <erikamerico1991@gmail.com>
2025-06-18 21:23:04 -0400

    add "title" (tooltip) to the input fields of bracket 7.
     reading where the team that currently is in the position was previously.
    
     make the input fields un-clickable.
    
     Will do this to all other brackets in the future.

--- src/components/bracket/team-count-brackets/7-team-bracket/SevenTeamBracket.tsx
+++ src/components/bracket/team-count-brackets/7-team-bracket/SevenTeamBracket.tsx
@@ -185,11 +185,17 @@ export default function SevenTeamBracket({
               className="team-input"
               value={renderTeamName(team4)}
               readOnly
+              title="4 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               value={renderTeamName(team5)}
               readOnly
+              title="5 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
               Match 1 <TrophyFilled onClick={() => showModal(1)} />
@@ -201,11 +207,17 @@ export default function SevenTeamBracket({
               className="team-input"
               value={renderTeamName(team2)}
               readOnly
+              title="2 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               value={renderTeamName(team7)}
               readOnly
+              title="7 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
               Match 2 <TrophyFilled onClick={() => showModal(2)} />
@@ -217,14 +229,20 @@ export default function SevenTeamBracket({
               className="team-input"
               value={renderTeamName(team3)}
               readOnly
+              title="3 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               value={renderTeamName(team6)}
               readOnly
+              title="6 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
-              Match 2 <TrophyFilled onClick={() => showModal(3)} />
+              Match 3 <TrophyFilled onClick={() => showModal(3)} />
             </span>
           </div>
         </div>
@@ -236,12 +254,22 @@ export default function SevenTeamBracket({
               className="team-input"
               value={renderTeamName(team1)}
               readOnly
+              title="1 Seed"
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               placeholder="Winner of 1"
               value={renderTeamName(matchResults[1].winner)}
               readOnly
+              title={
+                renderTeamName(matchResults[1].winner)
+                  ? "Winner of 1"
+                  : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
               Match 5 <TrophyFilled onClick={() => showModal(5)} />
@@ -254,12 +282,26 @@ export default function SevenTeamBracket({
               placeholder="Winner of 2"
               value={renderTeamName(matchResults[2].winner)}
               readOnly
+              title={
+                renderTeamName(matchResults[2].winner)
+                  ? "Winner of 2"
+                  : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               placeholder="Winner of 3"
               value={renderTeamName(matchResults[3].winner)}
               readOnly
+              title={
+                renderTeamName(matchResults[3].winner)
+                  ? "Winner of 3"
+                  : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
               Match 6 <TrophyFilled onClick={() => showModal(6)} />
@@ -273,12 +315,22 @@ export default function SevenTeamBracket({
             placeholder="Winner of 5"
             value={renderTeamName(matchResults[5].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[5].winner) ? "Winner of 5" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <input
             className="team-input"
             placeholder="Winner of 6"
             value={renderTeamName(matchResults[6].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[6].winner) ? "Winner of 6" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <span className="match-number">
             Match 10 <TrophyFilled onClick={() => showModal(10)} />
@@ -291,12 +343,26 @@ export default function SevenTeamBracket({
             placeholder="Winner of 10"
             value={renderTeamName(matchResults[10].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[10].winner)
+                ? "Winner of 10"
+                : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <input
             className="team-input"
             placeholder="Winner of losers"
             value={renderTeamName(matchResults[11].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[11].winner)
+                ? "Winner of losers"
+                : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <span className="match-number">
             Match 12 <TrophyFilled onClick={() => showModal(12)} />
@@ -319,12 +385,26 @@ export default function SevenTeamBracket({
                 value={renderTeamName(matchResults[12].winner)}
                 placeholder="Winner of 12"
                 readOnly
+                title={
+                  renderTeamName(matchResults[12].winner)
+                    ? "Winner of 12"
+                    : undefined
+                }
+                tabIndex={-1}
+                onMouseDown={(e) => e.preventDefault()}
               />
               <input
                 className="team-input"
                 value={renderTeamName(matchResults[12].loser)}
                 placeholder="Loser of 12 (if necessary)"
                 readOnly
+                title={
+                  renderTeamName(matchResults[12].loser)
+                    ? "loser of 12"
+                    : undefined
+                }
+                tabIndex={-1}
+                onMouseDown={(e) => e.preventDefault()}
               />
               <span className="match-number">
                 Match 13 <TrophyFilled onClick={() => showModal(13)} />
@@ -366,12 +446,22 @@ export default function SevenTeamBracket({
             placeholder="Loser of 2"
             value={renderTeamName(matchResults[2].loser)}
             readOnly
+            title={
+              renderTeamName(matchResults[2].loser) ? "loser of 2" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <input
             className="team-input"
             placeholder="Loser of 3"
             value={renderTeamName(matchResults[3].loser)}
             readOnly
+            title={
+              renderTeamName(matchResults[3].loser) ? "loser of 3" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <span className="match-number">
             Match 4 <TrophyFilled onClick={() => showModal(4)} />
@@ -386,12 +476,22 @@ export default function SevenTeamBracket({
               placeholder="Loser of 6"
               value={renderTeamName(matchResults[6].loser)}
               readOnly
+              title={
+                renderTeamName(matchResults[6].loser) ? "loser of 6" : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               placeholder="Loser of 1"
               value={renderTeamName(matchResults[1].loser)}
               readOnly
+              title={
+                renderTeamName(matchResults[1].loser) ? "loser of 1" : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
               Match 8 <TrophyFilled onClick={() => showModal(8)} />
@@ -404,12 +504,24 @@ export default function SevenTeamBracket({
               placeholder="Loser of 5"
               value={renderTeamName(matchResults[5].loser)}
               readOnly
+              title={
+                renderTeamName(matchResults[5].loser) ? "loser of 5" : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <input
               className="team-input"
               placeholder="Winner of 4"
               value={renderTeamName(matchResults[4].winner)}
               readOnly
+              title={
+                renderTeamName(matchResults[4].winner)
+                  ? "Winner of 4"
+                  : undefined
+              }
+              tabIndex={-1}
+              onMouseDown={(e) => e.preventDefault()}
             />
             <span className="match-number">
               Match 7 <TrophyFilled onClick={() => showModal(7)} />
@@ -424,12 +536,22 @@ export default function SevenTeamBracket({
             placeholder="Winner of 8"
             value={renderTeamName(matchResults[8].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[8].winner) ? "Winner of 8" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <input
             className="team-input"
             placeholder="Winner of 7"
             value={renderTeamName(matchResults[7].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[7].winner) ? "Winner of 7" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <span className="match-number">
             Match 9 <TrophyFilled onClick={() => showModal(9)} />
@@ -442,12 +564,22 @@ export default function SevenTeamBracket({
             placeholder="Loser of 10"
             value={renderTeamName(matchResults[10].loser)}
             readOnly
+            title={
+              renderTeamName(matchResults[10].loser) ? "Loser of 10" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <input
             className="team-input"
             placeholder="Winner of 9"
             value={renderTeamName(matchResults[9].winner)}
             readOnly
+            title={
+              renderTeamName(matchResults[9].winner) ? "Winner of 9" : undefined
+            }
+            tabIndex={-1}
+            onMouseDown={(e) => e.preventDefault()}
           />
           <span className="match-number">
             Match 11 <TrophyFilled onClick={() => showModal(11)} />
