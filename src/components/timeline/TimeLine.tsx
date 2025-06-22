5bfb73cddd3ec2cc978733ede67b63cb2813a8e8
Erik Olson <erikamerico1991@gmail.com>
2025-06-22 17:54:33 -0400

    improve cell phone ui- timeline

--- src/components/timeline/TimeLine.tsx
+++ src/components/timeline/TimeLine.tsx
@@ -1,62 +1,45 @@
-import { Timeline } from "antd";
 import "./timeLine.css";
+import { TrophyFilled } from "@ant-design/icons";
+
 const TimeLine = () => {
+  const items = [
+    { name: "Ofir & Michelle", date: "10/14/2023" },
+    { name: "Ofir & Michelle", date: "11/4/2023" },
+    { name: "Ofir & Michelle", date: "12/10/2023" },
+    { name: "Erik & Anna", date: "1/13/2024" },
+    { name: "Ofir & Michelle", date: "2/17/2024" },
+    { name: "Brittany & Zach", date: "3/23/2024" },
+    { name: "Brittany & Erik", date: "4/20/2024" },
+    { name: "Brittany & Michelle", date: "5/18/2024" },
+    { name: "Ofir & Erik", date: "6/21/2024" },
+    { name: "Rachel & Ofir", date: "6/7/2025" },
+    { name: "TBD", date: "TBD" },
+  ];
+
   return (
     <div className="timeline-page">
       <div className="main-container">
         <div id="history-container">
           <h2 className="champ-title">Past Champions</h2>
-          <Timeline
-            mode={"right"}
-            className="timeline"
-            items={[
-              {
-                label: "10/14/2023",
-                children: "Ofir & Michelle",
-              },
-              {
-                label: "11/4/2023",
-                children: "Ofir & Michelle",
-              },
-              {
-                label: "12/10/2023",
-                children: "Ofir & Michelle",
-              },
-              {
-                label: "1/13/2024",
-                children: "Erik & Anna",
-              },
-              {
-                label: "2/17/2024",
-                children: "Ofir & Michelle",
-              },
-              {
-                label: "3/23/2024",
-                children: "Brittany & Zach",
-              },
-              {
-                label: "4/20/2024",
-                children: "Brittany & Erik",
-              },
-              {
-                label: "5/18/2024",
-                children: "Brittany & Michelle",
-              },
-              {
-                label: "6/21/2024",
-                children: "Ofir & Erik",
-              },
-
-              {
-                label: "6/7/2025",
-                children: "Rachel & Ofir",
-              },
-              {
-                label: "TBD",
-                children: "TBD",
-              },
-            ]}
-          />
+          <table className="champions-table">
+            <tbody>
+              {items.map(({ name, date }) => (
+                <tr key={date}>
+                  <td>
+                    <span className="champ-glass">{name}</span>
+                  </td>
+                  <td>
+                    <span className="trophy-wrapper">
+                      <TrophyFilled className="champ-trophy" />
+                    </span>
+                  </td>
+                  <td>
+                    <span className="champ-glass">{date}</span>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
     </div>
