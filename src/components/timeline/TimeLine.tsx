46768260b980c5e68c55214213d0da94cec899f8
Erik Olson <erikamerico1991@gmail.com>
2025-06-24 10:49:40 -0400

    add champion images to timeline
    click on trophy to view them

--- src/components/timeline/TimeLine.tsx
+++ src/components/timeline/TimeLine.tsx
@@ -1,20 +1,95 @@
 import "./timeLine.css";
 import { TrophyFilled } from "@ant-design/icons";
+import { Modal, message } from "antd";
+import { useState } from "react";
+
+interface Champion {
+  name: string;
+  date: string;
+  photo?: React.ReactNode;
+}
 
 const TimeLine = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selected, setSelected] = useState<Champion | null>(null);
+
   const items = [
     { name: "TBD", date: "TBD" },
-    { name: "Rachel & Ofir", date: "6/7/2025" },
-    { name: "Ofir & Erik", date: "6/21/2024" },
-    { name: "Brittany & Michelle", date: "5/18/2024" },
-    { name: "Brittany & Erik", date: "4/20/2024" },
-    { name: "Brittany & Zach", date: "3/23/2024" },
-    { name: "Ofir & Michelle", date: "2/17/2024" },
-    { name: "Erik & Anna", date: "1/13/2024" },
+    {
+      name: "Rachel & Ofir",
+      date: "6/7/2025",
+      photo: (
+        <img src="assets/ofirRachelPNG.png" alt="" style={{ width: "300px" }} />
+      ),
+    },
+    {
+      name: "Ofir & Erik",
+      date: "6/21/2024",
+      photo: (
+        <img src="assets/ofir&erik.png" alt="" style={{ width: "300px" }} />
+      ),
+    },
+    {
+      name: "Brittany & Michelle",
+      date: "5/18/2024",
+      photo: (
+        <img
+          src="assets/brittany&michelle.png"
+          alt=""
+          style={{ width: "300px" }}
+        />
+      ),
+    },
+    {
+      name: "Brittany & Erik",
+      date: "4/20/2024",
+      photo: (
+        <img src="assets/erik_britt.png" alt="" style={{ width: "300px" }} />
+      ),
+    },
+    {
+      name: "Brittany & Zach",
+      date: "3/23/2024",
+      photo: (
+        <img src="assets/britt&zach.png" alt="" style={{ width: "300px" }} />
+      ),
+    },
+    {
+      name: "Ofir & Michelle",
+      date: "2/17/2024",
+    },
+    {
+      name: "Erik & Anna",
+      date: "1/13/2024",
+
+      photo: <img src="assets/1-13.png" alt="" style={{ width: "300px" }} />,
+    },
     { name: "Ofir & Michelle", date: "12/10/2023" },
-    { name: "Ofir & Michelle", date: "11/4/2023" },
-    { name: "Ofir & Michelle", date: "10/14/2023" },
+    {
+      name: "Ofir & Michelle",
+      date: "11/4/2023",
+      photo: <img src="assets/11-4.png" alt="" style={{ width: "300px" }} />,
+    },
+    {
+      name: "Ofir & Michelle",
+      date: "10/14/2023",
+    },
   ];
+
+  const openModal = (item: Champion) => {
+    if (!item.photo) {
+      message.warning(`No champion photo for this match. Do you have one?`);
+      return;
+    }
+
+    setSelected(item);
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelected(null);
+  };
 
   return (
     <div className="timeline-page">
@@ -23,18 +98,21 @@ const TimeLine = () => {
           <h2 className="champ-title">Past Champions</h2>
           <table className="champions-table">
             <tbody>
-              {items.map(({ name, date }) => (
-                <tr key={date}>
+              {items.map((item) => (
+                <tr key={item.date}>
                   <td>
-                    <span className="champ-glass">{name}</span>
+                    <span className="champ-glass">{item.name}</span>
                   </td>
                   <td>
                     <span className="trophy-wrapper">
-                      <TrophyFilled className="champ-trophy" />
+                      <TrophyFilled
+                        onClick={() => openModal(item)}
+                        className="champ-trophy"
+                      />
                     </span>
                   </td>
                   <td>
-                    <span className="champ-glass">{date}</span>
+                    <span className="champ-glass">{item.date}</span>
                   </td>
                 </tr>
               ))}
@@ -42,6 +120,16 @@ const TimeLine = () => {
           </table>
         </div>
       </div>
+      <Modal
+        className="custom-modal"
+        centered
+        open={isOpen}
+        onOk={closeModal}
+        closable={false}
+        cancelButtonProps={{ style: { display: "none" } }}
+      >
+        {selected?.photo || <p>No photo available</p>}
+      </Modal>
     </div>
   );
 };
