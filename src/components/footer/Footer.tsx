f0d196468dbd0742f3a48b5cf6e88934872cf85a
Erik Olson <erikamerico1991@gmail.com>
2025-06-27 14:10:01 -0400

    create chat logo in footer
    when clicked, open chat drawer

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,7 +1,14 @@
 import { Row, Col } from "antd";
+import { MessageTwoTone } from "@ant-design/icons";
+import ChatBox from "./ChatBox";
 import "./footer.css";
+import { useState } from "react";
 
 const Footer = () => {
+  const [open, setOpen] = useState(false);
+  const showDrawer = () => setOpen(true);
+  const onClose = () => setOpen(false);
+
   return (
     <div id="footer-container">
       <Row gutter={{ xs: 8, sm: 16, md: 24, lg: 32 }}>
@@ -9,6 +16,21 @@ const Footer = () => {
           <img id="footer-logo" src="assets/trophy.png" alt="" />
         </Col>
       </Row>
+
+      <span
+        className="footer-messaging-logo-container"
+        onClick={showDrawer}
+        style={{ cursor: "pointer" }}
+        role="button"
+        aria-label="Open chat"
+      >
+        <MessageTwoTone
+          twoToneColor={["black", "#866900"]}
+          className="footer-messaging-logo"
+        />
+      </span>
+
+      <ChatBox open={open} onClose={onClose} />
     </div>
   );
 };
